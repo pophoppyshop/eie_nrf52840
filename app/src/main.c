33c1126d476327cb230182966d880fc3571b787d
Li Tan <84640816+pophoppyshop@users.noreply.github.com>
2025-10-02 20:02:18 -0600

    Make LED blink

--- app/src/main.c
+++ app/src/main.c
@@ -1,9 +1,31 @@
+/*
+main.c
+*/
+
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-int main(void) {
-    while (1) {
+#define LED0_NODE DT_ALIAS(led0)
 
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+int main(void) {
+    int ret;
+
+    if (!gpio_is_ready_dt(&led0)) {
+        return -1;
+    }
+
+    ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+
+    if (ret < 0) {
+        return ret;
+    }
+
+    while (1) {
+        gpio_pin_toggle_dt(&led0);
+
+        k_msleep(1000);
     }
 
     return 0;
