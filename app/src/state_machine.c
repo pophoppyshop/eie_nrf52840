1ba553c471b6fc3927774d060a8d63cca73f9ed8
Li Tan <robloxcity88@gmail.com>
2026-01-02 17:34:21 -0700

    Finished ON_HOLD, all complete

--- app/src/state_machine.c
+++ app/src/state_machine.c
@@ -42,6 +42,13 @@ typedef struct {
     int code_index;             // index for code input (0 to 7)
     int string_index;           // index for string input (0 to 99)
     bool LED0_state, LED1_state;
+
+    // For ON_HOLD state
+    uint8_t current_duty_cycle;
+    enum machine_states previous_state;
+    int hold_duration_direction;          // in miliseconds / +-1
+    int counter;                          // miliseconds counter
+
 } state_machine_t;
 
 /*
@@ -58,11 +65,16 @@ static state_machine_t machine;
 
 void state_machine_init() {
     // Initialize variables
+    memset(machine.string_buffer, 0, sizeof(machine.string_buffer));
     machine.current_code = 0;
     machine.code_index = 7;
     machine.string_index = 0;
     machine.LED0_state = false, machine.LED1_state = false;
-    memset(machine.string_buffer, 0, sizeof(machine.string_buffer));
+    
+    machine.current_duty_cycle = 0;
+    machine.previous_state = ENTER_CODE;
+    machine.hold_duration_direction = 0;
+    machine.counter = 0;
 
     // Initial state
     smf_set_initial(SMF_CTX(&machine), &states[ENTER_CODE]);
@@ -88,15 +100,54 @@ void add_to_code(int bit){
     }
 }
 
+void clear_string() {
+    if (BTN_check_clear_pressed(BTN2)) {
+        machine.string_index = 0;
+        memset(machine.string_buffer, 0, sizeof(machine.string_buffer));
+
+        smf_set_state(SMF_CTX(&machine), &states[ENTER_CODE]);
+    }
+}
+
+void set_all_pwm(){
+    LED_pwm(LED0, machine.current_duty_cycle);
+    LED_pwm(LED1, machine.current_duty_cycle);
+    LED_pwm(LED2, machine.current_duty_cycle);
+    LED_pwm(LED3, machine.current_duty_cycle);
+}
+
+void add_hold_duration(enum machine_states state) {
+    if (BTN_is_pressed(BTN0) && BTN_is_pressed(BTN1)) {
+        machine.hold_duration_direction++;
+    }
+    else if (machine.hold_duration_direction != 0){
+        // Reset duration counter
+        machine.hold_duration_direction = 0;
+    }
+
+    // Check if held for 3s and switch to ON_HOLD
+    if (machine.hold_duration_direction >= 3000) {
+        machine.previous_state = state;
+
+        smf_set_state(SMF_CTX(&machine), &states[ON_HOLD]);
+    }
+}
+
 
 /*
     STATE: ENTER_CODE
 */
 static void enter_code_entry(void* o) {
     LED_blink(LED3, LED_1HZ);
+
+    // LED2 on once
+    if (machine.code_index == -1)
+        LED_set(LED2, true);
 }
 
 static enum smf_state_result enter_code_run(void* o) {
+    add_hold_duration(ENTER_CODE);
+
     // Turn on LED0 when BTN0 pressed -----------------
     if (BTN_is_pressed(BTN0) && !machine.LED0_state) {
         LED_set(LED0, true);
@@ -165,6 +216,8 @@ static void save_string_entry(void* o) {
 }
 
 static enum smf_state_result save_string_run(void* o) {
+    add_hold_duration(SAVE_STRING);
+
     // Turn on LED0 when BTN0 pressed -----------------
     if (BTN_is_pressed(BTN0) && !machine.LED0_state) {
         LED_set(LED0, true);
@@ -202,12 +255,7 @@ static enum smf_state_result save_string_run(void* o) {
     }
 
     // Reset string when BTN2 pressed ------------------
-    if (BTN_check_clear_pressed(BTN2)) {
-        machine.string_index = 0;
-        memset(machine.string_buffer, 0, sizeof(machine.string_buffer));
-
-        smf_set_state(SMF_CTX(&machine), &states[ENTER_CODE]);
-    }
+    clear_string();
 
     // Save string when BTN3 pressed -------------------
     if (BTN_check_clear_pressed(BTN3)) {
@@ -226,6 +274,8 @@ static void send_monitor_entry(void* o) {
 }
 
 static enum smf_state_result send_monitor_run(void* o) {
+    add_hold_duration(SEND_MONITOR);
+
     // Turn on LED0 when BTN0 pressed -----------------
     if (BTN_is_pressed(BTN0) && !machine.LED0_state) {
         LED_set(LED0, true);
@@ -253,17 +303,13 @@ static enum smf_state_result send_monitor_run(void* o) {
     }
 
     // Clear string when BTN2 pressed ------------------
-    if (BTN_check_clear_pressed(BTN2)) {
-        machine.string_index = 0;
-        memset(machine.string_buffer, 0, sizeof(machine.string_buffer));
-
-        smf_set_state(SMF_CTX(&machine), &states[ENTER_CODE]);
-    }
+    clear_string();
 
     // Send to serial monitor when BTN3 pressed -------------------
     if (BTN_check_clear_pressed(BTN3)) {
         printk("String: ");
 
+        // Use char pointer to print
         char* char_ptr = (char*) machine.string_buffer;
 
         printk("%s", char_ptr);
@@ -278,9 +324,49 @@ static enum smf_state_result send_monitor_run(void* o) {
     STATE: ON_HOLD
 */
 static void on_hold_entry(void* o) {
+    set_all_pwm();
 
+    machine.counter = 1015;
 }
 
 static enum smf_state_result on_hold_run(void* o) {
+    // Main loop start after a short delay 
+    if (machine.counter <= 15) {
+        // Change direction
+        if (machine.current_duty_cycle >= 100) {
+            machine.hold_duration_direction = -1;
+        }
+        else if (machine.current_duty_cycle <= 0) {
+            machine.hold_duration_direction = 1;
+        }
+
+        // Slows down the pulsing
+        if (machine.counter >= 15) {
+            machine.current_duty_cycle += machine.hold_duration_direction;
+            machine.counter = 0;
+        }
+        
+        machine.counter++;
+        set_all_pwm();
+
+        // Switch to previous state when any button pressed
+        if (BTN_is_pressed(BTN0) ||
+            BTN_is_pressed(BTN1) || 
+            BTN_is_pressed(BTN2) || 
+            BTN_is_pressed(BTN3)) { 
+                machine.current_duty_cycle = 0;
+                machine.counter = 0;
+                
+                set_all_pwm();
+
+                machine.LED0_state = false, machine.LED1_state = false;
+
+                smf_set_state(SMF_CTX(&machine), &states[machine.previous_state]);
+        }
+    }
+    else {
+        machine.counter--;
+    }
+
     return SMF_EVENT_HANDLED;
 }
