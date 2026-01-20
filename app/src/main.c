4196757c400856ffaf9c319edc07af1bac9cf9ec
Li Tan <robloxcity88@gmail.com>
2026-01-20 14:49:12 -0700

    Finish BLE_central lesson

--- app/src/main.c
+++ app/src/main.c
@@ -19,6 +19,10 @@
 
 #define SLEEP_MS 1
 
+static struct bt_uuid_16 discover_uuid = BT_UUID_INIT_16(0);
+static struct bt_gatt_discover_params discover_params;
+static struct bt_gatt_subscribe_params subscribe_params;
+
 static struct bt_conn* my_connection;
 
 static struct bt_uuid_128 BLE_CUSTOM_SERVICE_UUID = 
@@ -26,6 +30,75 @@ BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x11111111, 0x2222, 0x3333, 0x4444, 0x000000
 
 static struct bt_uuid_128 BLE_CUSTOM_CHARACTERISTIC_UUID = 
 BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x11111111, 0x2222, 0x3333, 0x4444, 0x000000000002));
+
+
+static uint8_t notify_func(struct bt_conn* conn, struct bt_gatt_subscribe_params* params,
+                           const void* data, uint16_t length) {
+  if (!data) {
+    printk("[UNSUBSCRIBED]\n");
+    params->value_handle = 0U;
+    return BT_GATT_ITER_STOP;
+  }
+
+  printk("[NOTIFICATION] data %p length %u\n", data, length);
+  for (int i = 0; i < MIN(length, 16); i++) {
+    printk(" 0x%02X", ((uint8_t*)data)[i]);
+  }
+  printk("\n");
+
+  return BT_GATT_ITER_CONTINUE;
+}
+
+static uint8_t discover_func(struct bt_conn* conn, const struct bt_gatt_attr* attr,
+                             struct bt_gatt_discover_params* params) {
+  int err;
+
+  if (!attr) {
+    printk("Discover complete\n");
+    (void)memset(params, 0, sizeof(*params));
+    return BT_GATT_ITER_STOP;
+  }
+
+  printk("[ATTRIBUTE] handle %u\n", attr->handle);
+
+  if (!bt_uuid_cmp(discover_params.uuid, &BLE_CUSTOM_SERVICE_UUID.uuid)) {
+    discover_params.uuid = &BLE_CUSTOM_CHARACTERISTIC_UUID.uuid;
+    discover_params.start_handle = attr->handle + 1;
+    discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
+
+    err = bt_gatt_discover(conn, &discover_params);
+    if (err) {
+      printk("Discover failed (err %d)\n", err);
+    }
+  } else if (!bt_uuid_cmp(discover_params.uuid, &BLE_CUSTOM_CHARACTERISTIC_UUID.uuid)) {
+    memcpy(&discover_uuid, BT_UUID_GATT_CCC, sizeof(discover_uuid));
+    discover_params.uuid = &discover_uuid.uuid;
+    discover_params.start_handle = attr->handle + 2;
+    discover_params.type = BT_GATT_DISCOVER_DESCRIPTOR;
+    subscribe_params.value_handle = bt_gatt_attr_value_handle(attr);
+
+    err = bt_gatt_discover(conn, &discover_params);
+    if (err) {
+      printk("Discover failed (err %d)\n", err);
+    }
+  } else {
+    subscribe_params.notify = notify_func;
+    subscribe_params.value = BT_GATT_CCC_NOTIFY;
+    subscribe_params.ccc_handle = attr->handle;
+
+    err = bt_gatt_subscribe(conn, &subscribe_params);
+    if (err && err != -EALREADY) {
+      printk("Subscribe failed (err %d)\n", err);
+    } else {
+      printk("[SUBSCRIBED]\n");
+    }
+
+    return BT_GATT_ITER_STOP;
+  }
+
+  return BT_GATT_ITER_STOP;
+}
+
 
 static void ble_on_device_connected(struct bt_conn* conn, uint8_t err) {
   if (err == 0 && conn == my_connection) {
@@ -37,6 +110,22 @@ static void ble_on_device_connected(struct bt_conn* conn, uint8_t err) {
     bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
     printk("Connected: %s\n", addr);
+
+    discover_params.uuid = &BLE_CUSTOM_SERVICE_UUID.uuid;
+    discover_params.func = discover_func;
+    discover_params.start_handle = BT_ATT_FIRST_ATTRIBUTE_HANDLE;
+    discover_params.end_handle = BT_ATT_LAST_ATTRIBUTE_HANDLE;
+    discover_params.type = BT_GATT_DISCOVER_PRIMARY;
+
+    err = bt_gatt_discover(my_connection, &discover_params);
+    if (err) {
+      printk("Discover failed(err %d)\n", err);
+      return;
+    }
+  }
+  else
+  {
+    printk("Connection failed (err %u)\n", err);
   }
 }
 
@@ -85,7 +174,7 @@ static void ble_on_advertisement_received(const bt_addr_le_t* addr, int8_t rssi,
   if (strcmp(name, "0591") == 0)
   {
     bt_le_scan_stop();
-
+   
     int result = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_DEFAULT, &my_connection);
 
     if (result != 0) {
