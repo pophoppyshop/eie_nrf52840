30ee69e816c639de9af30902784f8b671472fe9f
Li Tan <robloxcity88@gmail.com>
2026-01-29 17:23:37 -0700

    Added touch screen but touch status is only 32

--- app/src/main.c
+++ app/src/main.c
@@ -9,6 +9,7 @@
 #include <zephyr/drivers/spi.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/drivers/i2c.h>
 
 #include "BTN.h"
 #include "LED.h"
@@ -20,11 +21,43 @@
 #define CMD_ROW_ADDRESS_SET 0x2B
 #define CMD_MEMORY_WRITE 0x2C
 
-#define SLEEP_MS 1
+#define SLEEP_MS 100
+#define ADDR 0x38
+#define TD_STATUS 0x02
+#define P1_XH 0x03
+#define P1_XL 0x04
+#define P1_YH 0x05
+#define P1_YL 0x06
+
+#define TOUCH_EVENT_MASK 0xC0
+#define TOUCH_EVENT_SHIFT 6
+
+#define TOUCH_POS_MSB_MASK 0x0F
+
+typedef enum {
+  TOUCH_EVENT_PRESS_DOWN = 0b00u,
+  TOUCH_EVENT_LIFT_UP = 0b01u,
+  TOUCH_EVENT_CONTACT = 0b10u,
+  TOUCH_EVENT_NO_EVENT = 0b11u
+} touch_event_t;
 
 #define ARDUINO_SPI_NODE DT_NODELABEL(arduino_spi)
 #define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
 
+// TOUCH SCREEN
+#define ARDUINO_I2C_NODE DT_NODELABEL(arduino_i2c)
+static const struct device* dev2 = DEVICE_DT_GET(ARDUINO_I2C_NODE);
+
+void touch_control_cmd_rsp(uint8_t cmd, uint8_t* rsp) {
+  struct i2c_msg cmd_rsp_msg[2] = {
+    {&cmd, 1, I2C_MSG_WRITE},
+    {rsp, 1, I2C_MSG_RESTART | I2C_MSG_READ | I2C_MSG_STOP}
+  };
+
+  i2c_transfer(dev2, cmd_rsp_msg, 2, ADDR);
+}
+
+// LCD DISPLAY
 static const struct gpio_dt_spec dcx_gpio = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, dcx_gpios);
 static const struct spi_cs_control cs_ctrl = (struct spi_cs_control) {
   .gpio = GPIO_DT_SPEC_GET(ARDUINO_SPI_NODE, cs_gpios),
@@ -60,8 +93,15 @@ static void lcd_cmd(uint8_t cmd, struct spi_buf* data) {
   }
 }
 
+
 int main(void) {
   if(!device_is_ready(dev)) {
+    return 0;
+  }
+
+  // Touch screen
+  if (0 > i2c_configure(dev2, I2C_SPEED_SET(I2C_SPEED_FAST) | I2C_MODE_CONTROLLER)) {
+    printk("I2C configuration failed\n");
     return 0;
   }
 
@@ -105,6 +145,29 @@ int main(void) {
   lcd_cmd(CMD_MEMORY_WRITE, &color_data_buf);
 
   while(1) {
+    uint8_t touch_status;
+
+    touch_control_cmd_rsp(TD_STATUS, &touch_status);
+
+    printk("Touch status: %i\n", touch_status);
+
+    if (touch_status == 1) {
+      uint8_t x_pos_h;
+      uint8_t x_pos_l;
+      uint8_t y_pos_h;
+      uint8_t y_pos_l;
+
+      touch_control_cmd_rsp(P1_XH, &x_pos_h);
+      touch_control_cmd_rsp(P1_XL, &x_pos_l);
+      touch_control_cmd_rsp(P1_YH, &y_pos_h);
+      touch_control_cmd_rsp(P1_YL, &y_pos_l);
+
+      uint16_t x_pos = ((x_pos_h & TOUCH_POS_MSB_MASK) << 8) + x_pos_l;
+      uint16_t y_pos = ((y_pos_h & TOUCH_POS_MSB_MASK) << 8) + y_pos_l;
+
+      printk("Touch at %u, %u\n", x_pos, y_pos);
+    }
+
     k_msleep(SLEEP_MS);
   }
 	return 0;
